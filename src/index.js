abd43684ffae635e1f0031b5417980e5bd5885f4
dorothymuhonja <dorothymuhonja7@gmail.com>
2025-02-04 21:51:55 +0300

    add weather icon

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ function updateWeather(response) {
     let wind = response.data.wind.speed;
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
 
-
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
