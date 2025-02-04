c24ee15ddeb3eb42680bb5ca9459ea2d0665f0b1
dorothymuhonja <dorothymuhonja7@gmail.com>
2025-02-04 20:54:55 +0300

    consume api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+function updateWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#weather-app-city");
+
+    cityElement.innerHTML = response.data.city;
+
+    temperatureElement.innerHTML = Math.round(temperature);
+    
+}
+
+function searchCity(city) {
+    let apiKey = "e0ec6b874369tb90f96386aaf483o95a";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateWeather);
+    
+
+}
+
 function handleSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#weather-app-city");
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 
 
     
@@ -12,3 +30,5 @@ function handleSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", handleSubmit);
+
+searchCity("Nairobi");
