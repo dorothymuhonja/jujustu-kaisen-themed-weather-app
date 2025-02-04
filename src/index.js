6812750f14249e7c45c258cdc2f3dc281094027d
dorothymuhonja <dorothymuhonja7@gmail.com>
2025-02-04 19:42:28 +0300

    add basic search engine

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Yo!")
+function handleSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#weather-app-city");
+    cityElement.innerHTML = searchInput.value;
+
+
+    
+}
+
+
+let searchFormElement = document.querySelector("#search-form");
+
+searchFormElement.addEventListener("submit", handleSubmit);
